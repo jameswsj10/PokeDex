41b53715218a2da4c58bb2d34fc80341414d5b71
James <jameswsj@berkeley.edu>
2020-02-11 18:52:41 -0800

    adding starter code and beginning project

---
+++ Pokedex/Pokedex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by James Jung on 2/11/20.
+//  Copyright © 2020 James Jung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
